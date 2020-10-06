880cb82778e6b722faa64d9dca9c1ed3e082835a
Maïco Van den Bossche <maico.vandenbossche@student.romerocollege.be>
2020-10-06 10:03:54 +0200

    new programs

--- InitialNameDisplay/InitialNameDisplay/Program.cs
+++ InitialNameDisplay/InitialNameDisplay/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,8 +11,18 @@ namespace InitialNameDisplay
     {
         static void Main(string[] args)
         {
-            string FName, LName, Display;
-            Console.WriteLine(7 % 3);
+            string fName, lName, display, fNameUpper ;
+            Console.Write("Please enter your first name: ");
+            fName = Console.ReadLine();
+            fNameUpper = fName.Substring(0, 1);
+            Console.Write("Please enter your last name: ");
+            lName = Console.ReadLine();
+
+            display = fNameUpper.ToUpper() + ". " + lName.ToUpper();
+            Console.WriteLine(display);
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Press enter to exit...");
             Console.ReadLine();
         }
     }
---
+++ PasswordGenerator/PasswordGenerator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fName, lName, lNameUpper ,phoneNumber, display, displayLName;
+            float zipCode;
+            Console.Write("Please enter your first name: ");
+            fName = Console.ReadLine();
+            Console.Write("Please enter your last name: ");
+            lName = Console.ReadLine();
+            Console.Write("Please enter your phone number: ");
+            phoneNumber = Console.ReadLine();
+            Console.Write("Please enter your ZIP code: ");
+            zipCode = float.Parse(Console.ReadLine());
+            lNameUpper = lName.ToUpper();
+
+            displayLName = lNameUpper.Substring(0, 1) + lName.Substring(1, 1);
+            display = displayLName + displayLName.Substring(1, 0) + phoneNumber.Substring(1, 2) + Math.Pow(zipCode,2);
+            Console.WriteLine(display);
+
+            Console.ReadLine();
+        }
+    }
+}
